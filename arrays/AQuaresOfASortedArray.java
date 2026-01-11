44c781cc61110b0806a47c6d7ac465af0a872600
Barbare997 <Barbare997@users.noreply.github.com>
2026-01-11 22:33:10 +0400

    Add Squares of a Sorted Array solution

---
+++ arrays/AQuaresOfASortedArray.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class AQuaresOfASortedArray {
+    public int[] sortedSquares(int[] nums) {
+        for (int i=0; i<nums.length; i++) {
+            nums[i]=nums[i]*nums[i];
+        }
+        return Arrays.stream(nums).sorted().toArray();
+    }
+}
