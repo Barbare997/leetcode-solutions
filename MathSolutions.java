c8866a974168a84e02ce22cd8cc61cf4dc4c356a
Barbare997 <barbarepantskhava@gmail.com>
2026-01-28 14:43:23 +0400

    Add MathSolutions: Maximum 69 Number

--- MathSolutions.java
+++ MathSolutions.java
@@ -60,4 +60,11 @@ public int fib(int n) {
     }   
     return nums.length;
     }
+
+    //Maximum 69 Number
+    public int maximum69Number (int num) {
+        String numStr = String.valueOf(num);
+        numStr = numStr.replaceFirst("6", "9");
+        return Integer.parseInt(numStr);
+    }
 }
