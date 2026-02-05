c976e036a7ba4f9f02b0fb7ee40c096f93d22319
Barbare997 <barbarepantskhava@gmail.com>
2026-02-05 23:38:22 +0400

    Added Score of a String Solution

--- src/main/java/StringSolutiona.java
+++ src/main/java/StringSolutiona.java
@@ -37,4 +37,13 @@ public class StringSolutiona {
         }
         return result.toString();
     }
+
+    //Score of a String
+    public int scoreOfString(String s) {
+        int result = 0;
+        for (int i=1; i<s.length(); i++) {
+            result+=Math.abs(s.charAt(i)-s.charAt(i-1));
+        }
+        return result;
+    }
 }
