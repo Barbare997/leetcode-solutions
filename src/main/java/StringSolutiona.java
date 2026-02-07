33910226c6375f655ab036375063f3220b93051d
Barbare997 <barbarepantskhava@gmail.com>
2026-02-07 23:43:10 +0400

    Added Split a String in Balanced Strings solution

--- src/main/java/StringSolutiona.java
+++ src/main/java/StringSolutiona.java
@@ -70,5 +70,24 @@ public class StringSolutiona {
         return result.toString();
     }
 
+    //Split a String in Balanced Strings
+    public int balancedStringSplit(String s) {
+        int l = 0;
+        int r = 0;
+        int result = 0;
+        for (int i=0; i<s.length(); i++) {
+            if (s.charAt(i)=='L')
+                l++;
+            else
+                r++;
+            if (l==r && l!=0) {
+                result++;
+                l=0;
+                r=0;
+            }
+        }
+        return result;
+    }
+
 
 }
