f27a37766831bec195b7570475240090b8885dd5
Barbare997 <barbarepantskhava@gmail.com>
2026-02-09 20:47:58 +0400

    Added Number of Good Pairs solution

--- src/main/java/MathSolutions.java
+++ src/main/java/MathSolutions.java
@@ -115,4 +115,16 @@ public int fib(int n) {
         return sum;
     }
 
+    //Number of Good Pairs
+    public int numIdenticalPairs(int[] nums) {
+    int res = 0;
+    for (int i=0; i<nums.length; i++) {
+        for (int j=i+1; j<nums.length; j++) {
+            if (nums[i]==nums[j])
+                res++;
+        }
+    }
+    return res;
+    }
+
 }
